da0fbc13d4c4614783d0543abc87afc5f2664b4c
Fabio Delong <frdelong@gmail.com>
2025-08-30 02:12:31 -0300

    feat: implement session creation with email validation and password comparison

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
+import { z } from "zod"
 
 class SessionsController {
   async create(request: Request, response: Response){
-    response.json({ message: "ok" })
+    const bodySchema = z.object({
+      email: z.string().email({ message: "E-mail inválido" }),
+      password: z.string(),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email }})
+
+    if(!user){
+      throw new AppError("E-mail ou senha inválido", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched){
+      throw new AppError("E-mail ou senha inválido", 401)
+    }
+
+    response.json({ email, password })
   }
 }
 
