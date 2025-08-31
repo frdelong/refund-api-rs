cb2080d1d5377d10b35cdefb3dbc117d33d37f67
Fabio Delong <frdelong@gmail.com>
2025-08-31 13:28:46 -0300

    feat: add user authorization middleware for refund creation route

--- src/Routes/refunds-routes.ts
+++ src/Routes/refunds-routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express"
 
 import { RefundsController } from "@/controllers/refunds-controller"
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
 
 
 const refundsRoutes = Router()
 const refundsController = new RefundsController()
 
 // Rotas públicas.
-refundsRoutes.post("/", refundsController.create)
+refundsRoutes.post(
+  "/",
+  verifyUserAuthorization(["employee"]),
+  refundsController.create
+)
 
 export { refundsRoutes }
