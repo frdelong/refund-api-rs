c4d1735eed767efb9c1fbf2a2dc5fb33f954dab5
Fabio Delong <frdelong@gmail.com>
2025-08-31 16:03:02 -0300

    feat: enhance create refund method with validation schema using Zod

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from "express"
+import { z } from "zod"
 
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation"
+])
 class RefundsController {
-  async create(request: Request, response: Response){
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .trim()
+        .min(1, { message: "Informe o nome da solicitação" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+      filename: z.string().min(20)
+    })
+
     response.json({ message: "ok" })
   }
 }
