f9578b3e8616da03fb36ba20b828053d2ed2ccff
Fabio Delong <frdelong@gmail.com>
2025-08-31 16:34:47 -0300

    fix: format return statement in verifyUserAuthorization middleware

--- src/middlewares/verify-user-authorization.ts
+++ src/middlewares/verify-user-authorization.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import { AppError } from "@/utils/AppError"
 
 function verifyUserAuthorization(role: string[]) {
-  return(request: Request, response: Response, next: NextFunction) =>{
+  return (request: Request, response: Response, next: NextFunction) => {
     if (!request.user || !role.includes(request.user.role)) {
       throw new AppError("Unauthorized", 401)
     }
