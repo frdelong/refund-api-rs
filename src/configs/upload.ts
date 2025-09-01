f318bd02d4541afd53024cc7646271b901933941
Fabio Delong <frdelong@gmail.com>
2025-09-01 00:12:51 -0300

    feat: add multer configuration for file uploads

---
+++ src/configs/upload.ts
@@ -0,0 +1,29 @@
+import path from "node:path"
+import crypto from "node:crypto"
+import multer from "multer"
+
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
+const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads")
+
+const MAX_FILE_SIZE = 1024 * 1024 * 3
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
+
+const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString("hex")
+      const fileName = `${fileHash}-${file.originalname}`
+
+      return callback(null, fileName)
+    },
+  }),
+}
+
+export {
+  TMP_FOLDER,
+  UPLOADS_FOLDER,
+  MULTER,
+  MAX_FILE_SIZE,
+  ACCEPTED_IMAGE_TYPES,
+}
